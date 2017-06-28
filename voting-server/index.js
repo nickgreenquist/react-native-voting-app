3cf1274f009ff7a64cffa437fd057b3669018993
Nick Greenquist <ngreenquist@geekhive.com>
2017-06-28 12:47:08 -0400

    init commit

---
+++ voting-server/index.js
@@ -0,0 +1,13 @@
+import makeStore from './src/store';
+import startServer from './src/server';
+
+export const store = makeStore();
+startServer(store);
+
+console.log('server started successfully');
+
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
